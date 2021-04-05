31a36aa7e2078751f4a1f220f5bd99959422aa8f
dulfig <donulfig@gmail.com>
2021-04-05 13:40:20 -0500

    EC2 View

---
+++ src/test/java/com/bdd/aws/view/EC2View.java
@@ -0,0 +1,12 @@
+package com.bdd.aws.view;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EC2View {
+
+	public WebElement getAirbnb(WebDriver driver) {
+		return driver.findElement(By.xpath("//body/div[@id='aws-page-content']/main[1]/div[12]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/figure[1]/a[1]/img[1]"));
+	}
+}
