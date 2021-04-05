4f71af99f59ad0fb6c8e11f5fb5dedb583fbd126
shivani-senguttuvan <52843209+shivani-senguttuvan@users.noreply.github.com>
2021-04-05 14:55:20 -0500

    Update AWSController_Airbnb.java

--- src/test/java/com/bdd/aws/controller/AWSController_Airbnb.java
+++ src/test/java/com/bdd/aws/controller/AWSController_Airbnb.java
@@ -2,20 +2,27 @@ package com.bdd.aws.controller;
 
 import org.openqa.selenium.WebDriver;
 
+import com.bdd.aws.view.AirbnbView;
+
 public class AWSController_Airbnb {
+	
 	WebDriver driver;
+	AirbnbView airbnb;
 
 	// Prepare the page
 	public void setUp(WebDriver driver) {
+		
 		this.driver = driver;
+		airbnb = new AirbnbView(this.driver);
 
 	}
 
 	// Navigate to the page
 	public void toPage() {
-		// Click necessary buttons to get to page
-
+		
+		// navigate to YouTube page
+		airbnb.getSessionLink().click();
+	
 	}
-	// Additional methods required for testing
 
 }
