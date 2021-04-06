31cee33d78279115ca11ac7171b7eaf0403ff034
shivani-senguttuvan <52843209+shivani-senguttuvan@users.noreply.github.com>
2021-04-06 15:35:07 -0500

    Update HomeToCaseStudiesStepDefs.java

--- src/test/java/com/bdd/aws/stepdefs/HomeToCaseStudiesStepDefs.java
+++ src/test/java/com/bdd/aws/stepdefs/HomeToCaseStudiesStepDefs.java
@@ -10,34 +10,41 @@ import com.bdd.aws.controller.AWSController_Home;
 
 public class HomeToCaseStudiesStepDefs {
 	
+	private WebPage state;
+	WebDriver driver;
 	AWSController_Home AWSController_home;
+
+	public HomeToCaseStudiesStepDefs(WebPage state) {
+		this.state = state;
+		this.state.driver = driver;
+	}
 	
 	@Given("I am on the above page")
-	public void verifyCurrentPage(WebDriver driver) {
+	public void verifyCurrentPage() {
 		
 		// verify URL
-		Assert.assertEquals("https://aws.amazon.com/", driver.getCurrentUrl());
+		Assert.assertEquals("https://aws.amazon.com/", state.driver.getCurrentUrl());
 		// verify title
-		Assert.assertEquals("Amazon Web Services (AWS) -  Cloud Computing Services", driver.getTitle());
+		Assert.assertEquals("Amazon Web Services (AWS) -  Cloud Computing Services", state.driver.getTitle());
 		
 	}
 	
 	@When("I click on the Amazon EC2 link")
-	public void clickOnAmazonEC2Link(WebDriver driver) {
+	public void clickOnAmazonEC2Link() {
 		
 		AWSController_home = new AWSController_Home();
-		AWSController_home.setUp(driver);
+		AWSController_home.setUp(state.driver);
 		AWSController_home.toPage();
 		
 	}
 	
 	@Then("I am brought to a page that has Case Studies")
-	public void verifyRedirectionToCaseStudiesPage(WebDriver driver) {
+	public void verifyRedirectionToCaseStudiesPage() {
 		
 		// verify URL
 		Assert.assertEquals("https://aws.amazon.com/ec2/?hp=tile&so-exp=below", driver.getCurrentUrl());
 		// verify title 
-		Assert.assertEquals("Amazon EC2", driver.getTitle());
+		Assert.assertEquals("Amazon EC2", state.driver.getTitle());
 		
 	}
 
